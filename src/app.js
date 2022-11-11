855df77db5bbddbec3229868697b0ee5c7027e86
Jessica Oliveira de Souza Rissi <66426293+jessicaosouza@users.noreply.github.com>
2022-11-11 16:30:02 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ let dbclient;
 
 app.use(async (req, res, next) => {
     if(!connected) {
-        const uri = 'mongodb+srv://admina:nT2X2EA31xc0UjVh@cluster0.hp7msk8.mongodb.net/?retryWrites=true&w=majority'
+        const uri = ''
         dbclient = new MongoClient(uri)
         await dbclient.connect();
         collection = dbclient.db('users_db').collection('users')
